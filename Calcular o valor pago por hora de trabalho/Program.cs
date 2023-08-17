95ad7a8ee0d426d721ee1d4d4541b3be98efd036
vinicius.rlamim <vinicius.rlamim@SJC0562921W10-1>
2023-08-17 14:44:49 -0300

    Adicionar arquivos de projeto.

---
+++ Calcular o valor pago por hora de trabalho/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_o_valor_pago_por_hora_de_trabalho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double ValT, ValH, resultado;
+
+            Console.WriteLine("Digite o Salário:");
+            ValT = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite as horas trabalhadas:");
+            ValH = double.Parse(Console.ReadLine());
+            resultado = ValT / ValH;
+            Console.WriteLine("Valor pago por hora de trabalho = " + resultado);
+
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
